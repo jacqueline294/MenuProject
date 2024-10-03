4a839db498d8199c9409cc663265c12ade5c9037
jacqueline294 <jngigi86@gmail.com>
2024-10-03 14:04:28 +0200

    Initial Commit

---
+++ MenuProject/MenuProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuProjectApp.swift
+//  MenuProject
+//
+//  Created by jacqueline Ngigi on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct MenuProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
